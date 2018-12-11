1a9d1614d294e892416dfe72982e3a83ba70a3f7
Ashley Linares <linaresash@gmail.com>
2018-12-11 23:24:29 +0000

    just checking

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
     $("button").click(function() {
+        var num = 0;
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
@@ -9,6 +10,17 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        var q3score = dogsOrcats(q3Result);
+        var q2score = mood(q2Result);
+        var q1score = weather(q1Result);
+        totalScore = q3score + q2score + q1score;
+        if (totalScore === 22){
+            $(".result").html("You Are Craving Pizza!")
+        } else if (totalScore < 12){
+            $(".result").html("You Are Craving Salad!")
+        } else {
+            $(".result").html("You Are Craving SLEEP!")
+        }
     });
 
 });
@@ -30,17 +42,44 @@ $(document).ready(function() {
 
 // question
 
-var num = $("usrinput").val(num);
 
-function dogsOrcats(){
-    var usrinput = $("#question3").val();
-    if (usrinput === "dogs"){
-        return num + 3;
+function dogsOrcats(q3Result){
+    if (q3Result === "dogs"){
+        return 3;
     } 
-    else if (usrinput === "cats"){
-        return num + 2;
+    else if (q3Result === "cats"){
+        return 2;
     }
     else  {
-        return num + 1;
+        return 1;
     }
+}
+
+
+function mood(q2Result){
+    if (q2Result === "content"){
+        return 4;
+    } 
+    else if (q2Result === "agatated"){
+        return 3;
+    } else if (q2Result === "hungry"){
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+function weather(q3Result){
+    if (q3Result === "sunny"){
+        return 3;
+    }
+    else if (q3Result === "rainty"){
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+function congragulate() {
+    
 }
